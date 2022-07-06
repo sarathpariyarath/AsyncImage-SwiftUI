ef5d955c6206b6d83b0ff052defd69e9cb349319
sarathpariyarath <sarath.pariyarath@gmail.com>
2022-07-06 23:20:07 +0530

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Sarath P on 06/07/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
